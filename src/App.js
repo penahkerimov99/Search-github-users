39febf6d7e21883b9a12d8f005f8adbb1e0f5905
unknown <penahkerimov99@gmail.com>
2023-03-29 10:39:54 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { useRef } from 'react';
 import './App.css';
+import axios from "axios";
+import icon from './img/icon7.png'
 
 function App() {
+
+  const [username, setUsername] = useState("");
+
+  let ref = useRef(null)
+
+
+  var url1 = `https://api.github.com/users/${username}`
+  const url2 = `https://api.github.com/users/${username}/repos`
+
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(url1, {
+        headers: {
+          "Authorization": `Bearer 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`
+        }
+      });
+      const jsonData = await response.json();
+      setData(jsonData);
+    }
+    fetchData();
+  }, [url1]);
+
+
+
+  const [repo, setRepo] = useState({});
+
+  useEffect(() => {
+    async function fetchRepo() {
+      const response2 = await fetch(url2, {
+        headers: {
+          "Authorization": `Bearer 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`
+        }
+      });
+      const jsonData2 = await response2.json();
+      setRepo(jsonData2);
+    }
+    fetchRepo();
+  }, [url2]);
+
+
+
+  console.log(repo)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className='search-section'>
+          <h3>Search username</h3>
+          <div className='github-icon'>
+            <img src={icon} />
+          </div>
+          <input value={username} ref={ref} onChange={(e) => setUsername(e.target.value)} type='text' />
+        </div>
+      </div>
+
+      <div className='results'>
+        <div className='user-img'>
+          <img src={data.avatar_url} />
+        </div>
+        <h3 className='name-user'>{data.login}</h3>
+        <div className='repos'>
+          <ul>
+            {repo.length > 0 && repo?.map((repoItem, index) => (
+              <li key={index}>
+                <p>{repoItem.name}</p>
+                <p className='created'>Description: {repoItem.description}</p>
+                <p className='created'>Created: {repoItem.created_at}</p>
+                <p className='created'>Language: {repoItem.language}</p>
+                <p className='created'>Visibility: {repoItem.visibility}</p>
+                <p className='created'>Default branch: {repoItem.default_branch}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
